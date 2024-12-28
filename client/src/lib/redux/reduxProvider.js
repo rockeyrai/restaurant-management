3ede0991b978b39fea59be3ca39dc0ae97d80b63
Rockey C.R <rockeyrai234@gmail.com>
2024-12-28 15:40:21 +0545

    using redux-presist in redux

--- client/src/lib/redux/reduxProvider.js
+++ client/src/lib/redux/reduxProvider.js
@@ -1,12 +1,16 @@
 'use client'
 import React from 'react'
 import { Provider } from 'react-redux'
-import store from './store'
+import store, { persistor } from './store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const ReduxProvider = ({children}) => {
   return (
     <Provider store={store}>
-    {children}
+            <PersistGate loading={null} persistor={persistor}>
+            {children}
+            </PersistGate>
+
   </Provider>
   )
 }
--- client/src/lib/redux/store.js
+++ client/src/lib/redux/store.js
@@ -1,19 +1,40 @@
 // src/store.js
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Default to localStorage
 import userReducer from './slices/userSlices';
 import menuReducer from './slices/menuSlice';
 import orderReducer from './slices/orderSlice';
 import reservationReducer from './slices/reservationSlice';
 import settingsReducer from './slices/settingSlice';
 
+// Configuration for redux-persist
+const persistConfig = {
+  key: 'root',
+  storage,
+  // You can whitelist/blacklist specific slices here
+  whitelist: ['user', 'menu', 'orders', 'reservations', 'settings'], // Persist all slices
+};
+
+// Persist each reducer with redux-persist
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
+const persistedMenuReducer = persistReducer(persistConfig, menuReducer);
+const persistedOrderReducer = persistReducer(persistConfig, orderReducer);
+const persistedReservationReducer = persistReducer(persistConfig, reservationReducer);
+const persistedSettingsReducer = persistReducer(persistConfig, settingsReducer);
+
+// Create the store with persisted reducers
 const store = configureStore({
   reducer: {
-    user: userReducer,
-    menu: menuReducer,
-    orders: orderReducer,
-    reservations: reservationReducer,
-    settings: settingsReducer,
+    user: persistedUserReducer,
+    menu: persistedMenuReducer,
+    orders: persistedOrderReducer,
+    reservations: persistedReservationReducer,
+    settings: persistedSettingsReducer,
   },
 });
 
-export default store;
+// Create persistor to persist the state
+const persistor = persistStore(store);
+
+export { store, persistor };
